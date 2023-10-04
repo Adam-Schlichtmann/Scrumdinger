636c71cfc5970af4cf64e3f8e26ea9073a0e0c5e
Adam Schlichtmann <aschlichtmann@dmsi.com>
2023-10-04 08:42:20 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Adam Schlichtmann on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
